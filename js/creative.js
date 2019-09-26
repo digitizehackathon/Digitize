99dca9eef5ba6112561f3e3c76ab3b5d757e9eeb
briannahuang <41088835+briannahuang@users.noreply.github.com>
2019-09-25 19:19:48 -0700

    update timer

--- js/creative.js
+++ js/creative.js
@@ -80,7 +80,7 @@ const second = 1000,
     hour = minute * 60,
     day = hour * 24;
 
-let countDown = new Date('September 28, 2019 13:00:00').getTime(),
+let countDown = new Date('September 28, 2019 10:00:00').getTime(),
     x = setInterval(function () {
 
         let now = new Date().getTime(),
